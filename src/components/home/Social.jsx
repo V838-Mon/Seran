21ad1f9e6c2f57eb662155738696e6e019ffd4ee
V838-Mon <3025801055@qq.com>
2023-05-01 02:35:24 +0800

    .

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -8,12 +8,12 @@ const Social = () => {
         <i className="iconfont icon-bilibili"></i>
         </a>
         
-        <a href="https://github.com/" 
+        <a href="#" 
         className="home_social-icon" target='_blank'>
         <i className="iconfont icon-notion"></i>
         </a>
 
-        <a href="https://github.com/" 
+        <a href="https://github.com/V838-Mon" 
         className="home_social-icon" target='_blank'>
         <i class="uil uil-github-alt"></i>
         </a>
